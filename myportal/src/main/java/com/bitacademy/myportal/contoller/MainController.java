86fd1f21f51cb9cf0e50ea5dc8241507929f4afc
전혜지 <gpfp7@naver.com>
2021-04-13 17:06:18 +0900

    commit

--- myportal/src/main/java/com/bitacademy/myportal/contoller/MainController.java
+++ myportal/src/main/java/com/bitacademy/myportal/contoller/MainController.java
@@ -1,12 +1,18 @@
 package com.bitacademy.myportal.contoller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 @Controller
 public class MainController {
-	@RequestMapping("/main")
+
+	@RequestMapping("/hello")
 	public ModelAndView home() {
 		ModelAndView mav = new ModelAndView();
-		mav.addObject("message", "Hello, " + name);
+		
 		mav.setViewName("/WEB-INF/views/home.jsp");
+		
 		return mav;
 	}
 }
