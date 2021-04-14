6de05eff88b7f6771d1120a9273888a38fe47b0e
전혜지 <gpfp7@naver.com>
2021-04-14 15:00:26 +0900

    3-Tier Layer Skeleton

--- myportal/src/main/java/com/bitacademy/myportal/controller/MainController.java
+++ myportal/src/main/java/com/bitacademy/myportal/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.bitacademy.myportal.contoller;
+package com.bitacademy.myportal.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
